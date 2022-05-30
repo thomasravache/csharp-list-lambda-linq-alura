8af295665d25ddadd810227427e04949c9cc2adc
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-05-30 11:07:53 -0300

    feat: criar primeira lista

---
+++ CSharp_List_Lambda_Linq/ListExtensoes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_List_Lambda_Linq
+{
+    public static class ListExtensoes
+    {
+        public static void AdicionarVarios(List<int> lista, params int[] items)
+        {
+            foreach(int item in items)
+            {
+                lista.Add(item);
+            }
+        }
+    }
+}
--- CSharp_List_Lambda_Linq/Program.cs
+++ CSharp_List_Lambda_Linq/Program.cs
@@ -10,6 +10,22 @@ namespace CSharp_List_Lambda_Linq
     {
         static void Main(string[] args)
         {
+            List<int> idades = new List<int>();
+
+            idades.Add(1);
+            idades.Add(5);
+            idades.Add(6);
+
+            //idades.Remove(1);
+
+            ListExtensoes.AdicionarVarios(idades, 1, 2, 3, 4);
+
+            for(int i = 0; i < idades.Count; i++)
+            {
+                Console.WriteLine(idades[i]);
+            }
+
+            Console.ReadLine();
         }
     }
 }
